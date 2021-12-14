b818e5ba3eff63ce032d843d5cf1b0e12d5d8c13
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-14 06:13:55 -0700

    API Controller Class with Error for HTTP methods

--- CoreWebApiApp/Controllers/CategoryAPIController.cs
+++ CoreWebApiApp/Controllers/CategoryAPIController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreWebApiApp.Models;
+using CoreWebApiApp.Services;
 
 namespace CoreWebApiApp.Controllers
 {
@@ -11,5 +13,59 @@ namespace CoreWebApiApp.Controllers
     [ApiController]
     public class CategoryAPIController : ControllerBase
     {
+        private readonly IService<Category, int> _service;
+
+        public CategoryAPIController(IService<Category, int> service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> GetAsync()
+        {
+            var cats = await _service.GetAsync();
+            return Ok(cats);
+        }
+
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var cat = await _service.GetAsync(id);
+            return Ok(cat);
+        }
+
+        public async Task<IActionResult> PostAsync(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                var cat = await _service.CreateAsync(category);
+                return Ok(category);
+            }
+
+            return BadRequest(ModelState);
+
+        }
+
+        public async Task<IActionResult> PutAsync(int id , Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                var cat = await _service.UpdateAsync(id,category);
+                return Ok(category);
+            }
+
+            return BadRequest(ModelState);
+
+        }
+
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var res = await _service.DeleteAsync(id);
+            if (res)
+            {
+                return Ok($"Record deleted successfully {res}");
+            }
+
+            return NotFound($"Record Not Found");
+        }
+
     }
 }
