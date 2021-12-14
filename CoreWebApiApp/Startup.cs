0d243bb4f3f82d0147443c72f867062409a99151
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-14 06:36:07 -0700

    Default JSON Serialization

--- CoreWebApiApp/Startup.cs
+++ CoreWebApiApp/Startup.cs
@@ -29,8 +29,11 @@ namespace CoreWebApiApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
             services.AddDbContext<AppApiDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("AppConnection"));
