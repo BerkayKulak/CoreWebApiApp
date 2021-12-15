ffc65a7161062a45e5beaf2e15981f03fa293f87
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-15 05:07:29 -0700

    Creating AuthService class Part 3

--- CoreWebApiApp/Services/AuthService.cs
+++ CoreWebApiApp/Services/AuthService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using CoreWebApiApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CoreWebApiApp.Services
 {
@@ -48,6 +50,22 @@ namespace CoreWebApiApp.Services
             {
                 var secretKey = Convert.FromBase64String(configuration["JWTCoreSettings.SecretKey"]);
                 var expiryTime = Convert.ToInt32(configuration["JWTCoreSettings.ExpiryInMinuts"]);
+                IdentityUser appUser = new IdentityUser(user.UserName);
+                // Describe the JWT Token
+                var securityTokenDescriptor = new SecurityTokenDescriptor()
+                {
+                    Issuer = null,
+                    Audience = null,
+                    Subject = new ClaimsIdentity(new List<Claim>
+                    {
+                        new Claim("username",appUser.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(expiryTime),
+                    IssuedAt = DateTime.UtcNow,
+                    NotBefore = DateTime.UtcNow,
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKey),SecurityAlgorithms.HmacSha256Signature)
+                   
+                };
             }
             else
             {
