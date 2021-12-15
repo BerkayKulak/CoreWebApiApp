6864f43829671d3083cb8923811000bd47797745
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-15 00:24:54 -0700

    Creating class having Middleware Logic

---
+++ CoreWebApiApp/Middlewares/ExceptionMiddlewareLogic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace CoreWebApiApp.Middlewares
+{
+    public class ExceptionInfo
+    {
+        public int ExceptionId { get; set; }
+        public string ExceptionMessage { get; set; }
+    }
+    public class ExceptionMiddlewareLogic
+    {
+        private readonly RequestDelegate _request;
+        public ExceptionMiddlewareLogic(RequestDelegate request)
+        {
+            _request = request;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _request(context);
+            }
+            catch (Exception e)
+            {
+                await ExceptionLogic(context, e);
+            }
+        }
+
+        private async Task ExceptionLogic(HttpContext ctx , Exception ex)
+        {
+            ctx.Response.StatusCode = 500;
+
+            string message = ex.Message;
+
+            var exceptionInfo = new ExceptionInfo()
+            {
+                ExceptionId = ctx.Response.StatusCode,
+                ExceptionMessage = message
+            };
+
+            string responseMessage = JsonConvert.SerializeObject(exceptionInfo);
+
+            await ctx.Response.WriteAsync(responseMessage);
+        }
+    }
+}
